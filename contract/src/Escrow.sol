09936e7770d4909755ab382fd0d9f70430b75649
cryptedoji <ojiinnocent@gmail.com>
2025-06-02 14:00:18 +0100

    added more updates to escrow...

--- contract/src/Escrow.sol
+++ contract/src/Escrow.sol
@@ -1,22 +1,40 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import {IEcomm} from  "../lib/interfaces/IEcomm.sol";
+import {IEcomm} from "../lib/interfaces/IEcomm.sol";
 
 contract Escrow {
-    address public buyer;
-    address public seller;
+    address public owner;
     address public ecommercePlatform;
     IEcomm ecommInterface;
-    mapping(uint256 _userId => mapping(uint256 _paymentRef => uint256 _balance)) public userBalance;
-    constructor(address _ecomm) {
-        ecommercePlatform = _ecomm;
-        ecommInterface = IEcomm(_ecomm);
+    mapping(uint256 _userId => mapping(uint256 _paymentRef => uint256 _balance))
+        public userBalance;
+
+    constructor() {
+        owner = msg.sender;
+        // Assuming the contract deployer is the ecommerce platform
     }
 
-    function payForItems(uint _payRef, uint _userId) external  payable isCorrectFundsSent(_userId, _payRef) returns (bool) {
+    function setEcommercePlatform(
+        address _ecommercePlatform
+    ) external onlyOwner {
+        require(
+            _ecommercePlatform != address(0),
+            "Invalid ecommerce platform address"
+        );
+        ecommercePlatform = _ecommercePlatform;
+        ecommInterface = IEcomm(ecommercePlatform);
+    }
+
+    function payForItems(
+        uint _payRef,
+        uint _userId
+    ) external payable isCorrectFundsSent(_userId, _payRef) returns (bool) {
         require(msg.value > 0, "Payment must be greater than zero");
-        require(msg.value == ecommInterface.userCheckoutAmount(_userId), "Incorrect payment amount");
+        require(
+            msg.value == ecommInterface.userCheckoutAmount(_userId),
+            "Incorrect payment amount"
+        );
         // Logic to handle payment
         // For example, transfer funds to the seller
         // and emit an event for the transaction
@@ -29,8 +47,17 @@ contract Escrow {
         _;
         uint escrowBalAfter = address(this).balance;
         uint userBalAfter = userBalance[_userId][_payRef];
-        require(escrowBalAfter == escrowBalBefore + userBalAfter, "Incorrect funds sent to escrow");
+        require(
+            escrowBalAfter == escrowBalBefore + userBalAfter,
+            "Incorrect funds sent to escrow"
+        );
     }
 
-    
+    modifier onlyOwner() {
+        require(
+            msg.sender == ecommercePlatform,
+            "Only the ecommerce platform can call this function"
+        );
+        _;
+    }
 }
