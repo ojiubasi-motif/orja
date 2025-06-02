ca8f7707c1cc87b90bad8f3887b42b7558025a70
cryptedoji <ojiinnocent@gmail.com>
2025-06-02 14:41:23 +0100

    added scripting...

--- contract/script/EcommScript.s.sol
+++ contract/script/EcommScript.s.sol
@@ -2,11 +2,30 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Ecommerce} from "../src/Ecomm.sol";
+import {Escrow} from "../src/Escrow.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract EcommScript is Script {
+
+    uint256 private deployerPrivateKey;
+    address private deployer;
+    Ecommerce public ecomm;
+    Escrow public escrow;
+
+    function setUp() public {
+       deployerPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
+       deployer = vm.addr(deployerPrivateKey);
+    }
 
     function run() public {
-        vm.broadcast();
+        setUp();
+        vm.startBroadcast(deployer);
+        escrow = new Escrow();
+        ecomm = new Ecommerce(address(escrow));
+        escrow.setEcommercePlatform(address(ecomm));
+        vm.stopBroadcast();
+        console.log("Ecommerce contract deployed at:", address(ecomm));
+        console.log("Escrow contract deployed at:", address(escrow));
+        console.log("Deployer address:", deployer);
     }
 }
